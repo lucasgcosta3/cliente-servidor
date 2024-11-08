ae7d2e46f3b627d74c38c654e911a4aea775e7db
Lucas Costa <lg1352439@gmail.com>
2024-11-07 21:49:29 -0300

    Update reset_password.php

--- reset_password.php
+++ reset_password.php
@@ -13,7 +13,7 @@ if (isset($_POST['reset_password'])) {
 
     if ($new_password === $confirm_password) {
         if ($user->resetPassword($token, $new_password)) {
-            $success = "Senha redefinida com sucesso! <a href='login.php'>Clique aqui para fazer login</a>";
+            $success = "Senha redefinida com sucesso! Clique aqui para fazer login.";
         } else {
             $error = "Token inválido ou expirado.";
         }
@@ -21,6 +21,11 @@ if (isset($_POST['reset_password'])) {
         $error = "As senhas não correspondem.";
     }
 }
+
+// Armazena mensagens de sucesso ou erro em variáveis de sessão
+session_start();
+$_SESSION['success'] = $success;
+$_SESSION['error'] = $error;
 ?>
 
 <!DOCTYPE html>
@@ -31,8 +36,7 @@ if (isset($_POST['reset_password'])) {
   <title>Recuperação de senha</title>
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <script src="assets/js/script.js"></script>
-  <script src="assets/js/sweetAlert.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 </head>
 <body>
   <div class="main-login">
@@ -40,29 +44,41 @@ if (isset($_POST['reset_password'])) {
       <div class="right-login">
         <div class="card-login">
           <h1 style="text-align: center;">Redefinir Senha</h1>
-          <?php if ($error): ?>
-            <p style="color: red;"><?php echo $error; ?></p>
-        <?php endif; ?>
-        <?php if ($success): ?>
-            <p style="color: green;"><?php echo $success; ?></p>
-        <?php else: ?>
+
           <div class="textfield">
             <label for="new_password">Nova senha</label>
             <div class="input-wrapper">
-              <input type="password" name="new_password" id="new_password" placeholder="Digite a nova senha" required>
+              <input type="password" name="new_password" id="new_password" placeholder="Digite a nova senha" required
+                     pattern="(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d\W]{8,}"
+                     title="A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula e um número">
+              <i style="cursor: pointer;" class="fa-regular fa-eye toggle-password" onclick="verSenha('new_password')"></i>
             </div>
           </div>
           <div class="textfield">
-              <label for="confirm_password">Confirme a senha</label>
-              <div class="input-wrapper">
-                <input type="password" name="confirm_password" id="confirm_password" placeholder="Digite a senha novamente" required>   
-                <i style="cursor: pointer;" class="fa-regular fa-eye toggle-password" onclick="verSenha()"></i>
-              </div>
+            <label for="confirm_password">Confirme a senha</label>
+            <div class="input-wrapper">
+              <input type="password" name="confirm_password" id="confirm_password" placeholder="Digite a senha novamente" required>   
+              <i style="cursor: pointer;" class="fa-regular fa-eye toggle-password" onclick="verSenha('confirm_password')"></i>
             </div>
-        <button class="btn-login" style="margin: 25px" type="submit" name="reset_password">Salvar</button>
+          </div>
+          <button class="btn-login" style="margin: 25px" type="submit" name="reset_password">Salvar</button>
+        </div>
       </div>
     </form>
-    <?php endif; ?>
   </div>
+
+  <!-- Campos ocultos para passar mensagens -->
+  <input type="hidden" id="success-message" value="<?php echo isset($_SESSION['success']) ? $_SESSION['success'] : ''; ?>">
+  <input type="hidden" id="error-message" value="<?php echo isset($_SESSION['error']) ? $_SESSION['error'] : ''; ?>">
+
+  <?php
+    // Limpar as mensagens após capturá-las
+    unset($_SESSION['success']);
+    unset($_SESSION['error']);
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="assets/js/sweetAlert.js"></script>
+  <script src="assets/js/script.js"></script>
 </body>
 </html>
